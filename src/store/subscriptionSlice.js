2ef62d5ffc3bbedbddda4a9182515172bfb40a32
Hanen Riguen <rh.hanen@gmail.com>
2025-08-25 19:09:03 +0200

    Update subscriptionSlice.js

--- src/store/subscriptionSlice.js
+++ src/store/subscriptionSlice.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:9090/subscriptions';
+const API_URL = import.meta.env.VITE_API_URL + "/subscriptions"|| 'http://localhost:9090/subscriptions';
 
 
 const extractError = (error, fallback = 'Erreur inconnue') => {
